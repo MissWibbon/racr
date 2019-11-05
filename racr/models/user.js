19b80de4f5f0d4d253bd6f2706f2107285a15793
Michelle <wuebben.michelle@yahoo.com>
2019-11-04 21:18:24 -0600

    added seed race and index

---
+++ racr/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+​
+const userSchema = new Schema({
+    id: Number,
+    firstName: String,
+    lastName: String,
+    username: String,
+    email: String,
+    password: String,
+    city: String,
+    state: String,
+    country: { type: String, default: "USA" },
+    age: Number,
+    image: String,
+    date: { type: Date, default: Date.now }
+});
+​
+const User = mongoose.model("User", userSchema);
+​
+module.exports = User;
