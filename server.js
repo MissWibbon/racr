49fbcc4b81f3d1ffde12468cf7f9c3f8c974b215
tyfal <tfalcoff@gmail.com>
2019-11-06 21:07:00 -0600

    changing front end info on server js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(express.json());
 // Define API routes
 app.use(routes);
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/racrDB";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/RacrDB";
 mongoose.connect(MONGODB_URI)
 .then(() => console.log('Mongo Db connected'))
 .catch(err => console.log(err));
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static('racr/build'))
+   app.use(express.static('frontend/build'))
    app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, 'racr', 'build', 'index.html'))
+       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
    })
 }
 var PORT = process.env.PORT || 5000;
