a66700f9b9ee89b6a80f3536681018e98b6638da
Michelle <wuebben.michelle@yahoo.com>
2019-11-06 19:04:24 -0600

    added landing and home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, NavLink, Switch} from 'react-router-dom'
+import Landing from './landing'
+import Home from './'
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route to exaxt path = '/' component ={Landing}/>
+        <Route to exaxt path = '/home' component ={Home}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
