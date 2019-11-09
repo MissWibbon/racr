6c7ebbc147db4ae51578fcb8960e6a2058cabd7c
Michelle <wuebben.michelle@yahoo.com>
2019-11-09 10:44:20 -0600

    added style to user page and search box

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -1,16 +1,40 @@
-import React, {useContext}from 'react'
+import React, {useContext, useEffect, useState}from 'react'
 import {RaceContext} from './appstate'
+import ReactSearchBox from 'react-search-box'
+
 
 
 const User = () =>{
- const context = useContext(RaceContext)
- //users is an array of dummyinfo right now use an index of users  
- const {users, isLoading} = context
+    const context = useContext(RaceContext)
+    const {users, isLoading} = context
+    const [userState, setUserstate] = useState([]);
+ useEffect(() => {
+     setUserstate(users);
+ })
+//users is an array of dummyinfo right now use an index of users  
+ console.log(userState);
     return(
-        <div>
+        
+        <div id="profilePage">
+        <ReactSearchBox 
+        placeholder = "Placeholder"
+        value = "Doe"
+        data = {userState}
+        callback = {record => console.log(record)}
+        />
         {isLoading
         ?(
-            <div>{`${users[0].firstName} ${users[0].lastName}` }</div>
+            <div>
+                <div className="profileImage">
+                    <img className="imgSrc" src={`${users[0].image}`}/>
+                </div>
+                <div class="profileInfo">
+                    <div className="profileUserName">{`@${users[0].username}` }</div>
+                    <div className="profileName">{`${users[0].firstName} ${users[0].lastName}` }</div>
+                    <div className="profileLocation">{`${users[0].city}, ${users[0].state} ${users[0].country}` }</div>
+                    <div className="profileRunType">{`${users[0].raceType.charAt(0).toUpperCase()}` + `${users[0].raceType.slice(1)}`} Runner</div>
+                </div>
+            </div>
         ):
         <h2>Loading...</h2>
         }
