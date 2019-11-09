5dad3f3cbc46e1044ea7e88a77b7234ac38a0a69
Michelle <wuebben.michelle@yahoo.com>
2019-11-09 13:30:32 -0600

    added useeffect to user.js

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -8,14 +8,12 @@ const User = () =>{
     const context = useContext(RaceContext)
     const {users, isLoading} = context
     const [userState, setUserstate] = useState([]);
-
  useEffect(() => {
-     let tempUserArray = [];
+     let tempUserArray = []
     console.log(userState[0]);
     for(let i in users) {
         tempUserArray.push(users[i].username);
     }
-
      setUserstate(tempUserArray);
  }, [users])
 //users is an array of dummyinfo right now use an index of users  
@@ -35,17 +33,10 @@ const User = () =>{
                 <div className="profileImage">
                     <img className="imgSrc" src={`${users[0].image}`}/>
                 </div>
-
                 <div className="profileLocation">{`${users[0].city}, ${users[0].state} ${users[0].country}` }</div>
                 <div class="profileInfo">
                     <div className="profileUserName">{`@${users[0].username}` }</div>
                     <div className="profileName">{`${users[0].firstName} ${users[0].lastName}` }</div>
-
-                <div class="profileInfo">
-                    <div className="profileUserName">{`@${users[0].username}` }</div>
-                    <div className="profileName">{`${users[0].firstName} ${users[0].lastName}` }</div>
-                    <div className="profileLocation">{`${users[0].city}, ${users[0].state} ${users[0].country}` }</div>
-
                     <div className="profileRunType">{`${users[0].raceType.charAt(0).toUpperCase()}` + `${users[0].raceType.slice(1)}`} Runner</div>
                 </div>
             </div>
