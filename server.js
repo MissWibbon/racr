12a9d42f6bdd6e88dc39b69c547f0c195db100cf
Steven Daoud <steven.daoud@gmail.com>
2019-11-11 21:16:08 -0600

    updated

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+ }
+ 
 // Define API routes
 app.use(routes);
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
@@ -16,12 +21,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/RacrDB";
 mongoose.connect(MONGODB_URI)
 .then(() => console.log('Mongo Db connected'))
 .catch(err => console.log(err));
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static('client/build'));
-   app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-   })
-}
+
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
    console.log(`server connected to port ${PORT}`);
