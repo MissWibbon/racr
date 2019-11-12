1182bdf18fd24471e7ede370ed486dcba6b7bd12
Michelle <35238012+MissWibbon@users.noreply.github.com>
2019-11-11 20:24:53 -0600

    Revert "Timer"

--- routes/api/user.js
+++ routes/api/user.js
@@ -13,8 +13,5 @@ router
     .put(userController.update)
     .delete(userController.remove);
 
-<<<<<<< HEAD
-=======
 router.post('/login',userController.login)
->>>>>>> 7c43ded95dc23f833c5ee41f50e8298146be180a
 module.exports = router;
