78529d72df3a0deaaa5aff85b2a8d5443d82d61f
Michelle <wuebben.michelle@yahoo.com>
2019-11-12 22:19:31 -0600

    made navbar mobile responsive, added logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import {NavLink} from 'react-router-dom'
+import {RaceContext} from './appstate'
+
+
+
 
 const Navbar = () =>{
+    const context = useContext(RaceContext);
+    const {isAuth} = context;
 
     return(
-        <div className="navWrapper">
-            <NavLink className="navLink" exact to = '/home'><i class="fas fa-user"></i></NavLink>
-            <NavLink className="navLink" exact to = '/notifications'><i class="fas fa-bell"></i></NavLink>
-            <NavLink className="navLink" exact to = '/race'><i class="fas fa-running"></i></NavLink>
-            <NavLink className="navLink" exact to = '/users'><i class="fas fa-search"></i></NavLink>
-            <NavLink className="navLink" exact to = '/login'><i class="fas fa-sign-out-alt"></i></NavLink>
+        <div>
+            {isAuth
+            ?(
+            <div className="navWrapper">
+                <NavLink className="navLink" exact to = '/home'><i className="fas fa-user"></i></NavLink>
+                <NavLink className="navLink" exact to = '/notifications'><i className="fas fa-bell"></i></NavLink>
+                <NavLink className="navLink" exact to = '/race'><i className="fas fa-running"></i></NavLink>
+                <NavLink className="navLink" exact to = '/users'><i className="fas fa-search"></i></NavLink>
+                <NavLink className="navLink" exact to = '/login'><i className="fas fa-sign-out-alt"></i></NavLink>
+            </div>
+            
+            ): null}
         </div>
     )
 }
+
 
 export default Navbar
--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -22,20 +22,22 @@ const Landing =(props) =>{
                     console.log('not auth')
                 }
             })
-        console.log('this is where i send shit');
         
     }
     return(
+        <div>
         <div className="loginWrap">
+        <div className="logo"></div>
             <form id="login">
                 <label id="email">Email:</label>
                 <input {...email}type ='text' name = 'name'></input>
                 <label id="password">Password:</label>
                 <input {...password}type ='password' name = 'name'></input>
                 <div><button id="submitButton" onClick ={handleSubmit} type ='submit'>Submit</button></div>
-                <div class="registerLink"><Link to="/signup">Register</Link></div>
+                <div className="registerLink"><Link to="/signup">Register</Link></div>
             </form>
         </div>
+    </div>
     )
 }
 
