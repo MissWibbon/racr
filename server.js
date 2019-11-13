2160571e0f8aa1f386d375a11e557cd1c0ea7a69
boyangmatsapola <boyangbass@gmail.com>
2019-11-12 20:11:51 -0600

    files organized and up to date

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/RacrDB";
 mongoose.connect(MONGODB_URI)
 .then(() => console.log('Mongo Db connected'))
 .catch(err => console.log(err));
-<<<<<<< HEAD
-
-=======
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static('frontend/build'))
-   app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'))
-   })
-}
->>>>>>> cb059f9dfde951cbd995cd06962ba56a6c277a94
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
    console.log(`server connected to port ${PORT}`);
