c5093ce8d60e1f2fa1cd1c4136e98ef14d18f08c
Wuebben <mwueb001@e036l04mkmiwu0a.local>
2019-11-16 20:26:06 -0600

    latPlayer1 and longPlayer1 arrays update on change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,11 +14,14 @@ const Navbar = () =>{
             {isAuth
             ?(
             <div className="navWrapper">
-                <NavLink className="navLink" exact to = '/challengeform'><div className="racr-nav-icon"/></NavLink>
-                <NavLink className="navLink" exact to = '/home'><i className="fas fa-user"></i></NavLink>
-                <NavLink className="navLink" exact to = '/notifications'><i className="fas fa-bell"></i></NavLink>
-                <NavLink className="navLink" exact to = '/users'><i className="fas fa-search"></i></NavLink>
-                <NavLink className="navLink" exact to = '/login'><i className="fas fa-sign-out-alt"></i></NavLink>
+                <div className="linkWraps">
+                    <NavLink className="navLink" exact to = '/challengeform'><div className="racr-nav-icon small-logo"/></NavLink>
+                    <NavLink className="navLink" exact to = '/home'><i className="fas fa-user"></i></NavLink>
+                    <NavLink className="navLink" exact to = '/notifications'><i className="fas fa-bell"></i></NavLink>
+                    <NavLink className="navLink" exact to = '/users'><i className="fas fa-search"></i></NavLink>
+                    <NavLink className="navLink" exact to = '/friends'><i className="fas fa-users"></i></NavLink>
+                    <NavLink className="navLink" exact to = '/login'><i className="fas fa-sign-out-alt"></i></NavLink>
+                </div>
             </div>
             
             ): null}
--- client/src/components/friendpool.js
+++ client/src/components/friendpool.js
@@ -4,10 +4,17 @@ import {RaceContext} from './appstate'
 const FriendPool =({match}) => {
     const context = useContext(RaceContext);
     const {localUser, fetchOneUser, friends, setFriends } = context
+
+    useEffect(()=>{
+        if(localUser !== undefined){
+
+            fetchOneUser(localUser._id)
+        }
+    },[])
     console.log(friends)
     return(
-        <div>
-            {/* <p>boyang</p> */}
+        <div className="friend-list">
+    
         </div>
 
     )
