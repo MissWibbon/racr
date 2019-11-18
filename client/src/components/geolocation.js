19b000c5a4b924e6abc4fef635efbe3b500968be
Wuebben <mwueb001@e036l04mkmiwu0a.local>
2019-11-17 22:48:19 -0600

    added distance variable

--- client/src/components/geolocation.js
+++ client/src/components/geolocation.js
@@ -2,6 +2,7 @@ import React, {Component, useContext, useEffect, useState}from 'react'
 import {RaceContext} from './appstate'
 
 import { geolocated } from "react-geolocated";
+import { stat } from 'fs';
  
 // function calcDistance() {
 //     feet = 1;
@@ -11,7 +12,7 @@ import { geolocated } from "react-geolocated";
  
 
 const Demo = () => {
-    
+    let dist = 0;
     const [state, setState] = useState(0) 
    
 
@@ -22,10 +23,12 @@ const Demo = () => {
     //Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
     const distance = () => {
         var watchID = navigator.geolocation.watchPosition(function(position) {
-            setState(prevState =>(
+            setState(prevState =>
+                (
                 {
                     lat:position.coords.latitude, 
-                    long: position.coords.longitude
+                    long: position.coords.longitude,
+                    dist:  Math.sqrt( Math.pow((prevState.lat-position.coords.latitude), 2) + Math.pow((prevState.long-position.coords.longitude), 2))
                 }))
           });
             
@@ -39,7 +42,7 @@ const Demo = () => {
                 ?
                 (
                     <>
-                    <p>{state.lat}</p><p>{state.long}</p>
+                    <p>{state.lat}</p><p>{state.long}</p><p>{state.dist}</p>
                     </>
                 )
                 : null
