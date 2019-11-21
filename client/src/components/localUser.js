bd03e6da46ee3f84d614220f62883484261ad7f9
Wuebben <mwueb001@e036l04mkmiwu0a.local>
2019-11-20 22:23:16 -0600

    fixed local user div

--- client/src/components/localUser.js
+++ client/src/components/localUser.js
@@ -1,32 +1,23 @@
-import React, { useContext, useState } from 'react'
-import { RaceContext } from './appstate'
+import React, {useContext, useState}from 'react'
+import {RaceContext} from './appstate'
 import SearchBar from './searchbar';
 
-
-
-const LocalUser = (props) => {
+const LocalUser = (props) =>{
     const context = useContext(RaceContext)
-    const { users, localUser } = context
-    const { isLoading } = context
+    const {users,localUser} = context
+    const {isLoading} = context
     const searchbar = useSearchValue('')
-    return (
+    return(
         <div id="profilePage">
-            <SearchBar {...props}></SearchBar>
-            {isLoading
-                ? (
-                    <div>
-                        <div className="profileImage">
-                            <img className="imgSrc" src={`${localUser.image} `} alt="profile-img" />
-                            <div className="locationWrap">
-                                <div className="profileLocation">{`${localUser.city}, ${localUser.state} ${localUser.country}`}</div>
-                            </div>
-                        </div>
-                        <div className="profileInfo">
-                            <div className="profileUserName">{`${localUser.userName}`}</div>
-                            <div className="profileName">{`${localUser.firstName} ${localUser.lastName}`}</div>
-                            {/* <div className="profileRunType">{`${users[3].raceType.charAt(0).toUpperCase()}` + `${users[3].raceType.slice(1)}`} Runner</div> */}
-                        </div>
-
+        <SearchBar {...props}></SearchBar>
+        {isLoading
+        ?(
+            <div>
+                <div className="profileImage">
+                    <img className="imgSrc" src={`${localUser.image} `} alt="profile-img"/>
+                    <div className="locationWrap">
+                        <div className="profileLocation">{`${localUser.city}, ${localUser.state} ${localUser.country}` }</div>
+                    </div>
                 </div>
                 <div className="profileInfo">
                     <div className="profileUserName">@{`${localUser.userName}` }</div>
@@ -37,23 +28,21 @@ const LocalUser = (props) => {
         ):
         <h2>Loading...</h2>
         }
-
-        </div>
+    </div>
     )
 }
 
-const useSearchValue = (initialValue) => {
+const useSearchValue = (initialValue) =>{
     const [userState, setUserState] = useState(initialValue);
-    const handlevaluechange = (e) => {
+    const handlevaluechange =(e) =>{
         setUserState(e.target.value);
         console.log(e.target.value)
     }
     return {
         value: userState,
         onChange: handlevaluechange,
-
+       
     }
 }
-
 
 export default LocalUser
