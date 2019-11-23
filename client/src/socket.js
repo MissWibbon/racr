3d59ce0b394a9e093e3b332387b15c925a0bc0c7
boyangmatsapola <boyangbass@gmail.com>
2019-11-22 21:13:33 -0600

    adding socket_server var 2

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const socketURL = process.env.SOCKET_SERVER || 'http://localhost:5000' 
+const socketURL = 'https://mernracr.herokuapp.com' || 'http://localhost:5000' 
 
 export const socket = io(socketURL);
 //'https://mernracr.herokuapp.com'
