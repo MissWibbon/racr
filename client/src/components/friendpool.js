30882bdc62984f7d50604276128e00a4914cf027
Michelle <35238012+MissWibbon@users.noreply.github.com>
2021-06-19 12:51:04 -0500

    added username to friend list

--- client/src/components/friendpool.js
+++ client/src/components/friendpool.js
@@ -1,7 +1,7 @@
 
-import React , {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import API from '../utils/API'
-import {RaceContext} from './appstate'
+import { RaceContext } from './appstate'
 import FriendCard from './friendCard'
 import SearchBar from './searchbar'
 
@@ -9,59 +9,59 @@ import SearchBar from './searchbar'
 const FriendPool = (props) => {
     const context = useContext(RaceContext);
 
-    const {localUser, friends, isLoading} = context
-   const [pool , setPool] = useState([])
+    const { localUser, friends, isLoading } = context
+    const [pool, setPool] = useState([])
 
 
-   const getfriends =() =>{
+    const getfriends = () => {
 
-       if(localUser ===undefined){
-           return false
+        if (localUser === undefined) {
+            return false
 
-       } else{
-               
-               localUser.friends.map(friend =>{
+        } else {
+
+            localUser.friends.map(friend => {
                 API.getOneUser(friend)
-                    .then(res =>{
+                    .then(res => {
                         console.log(res.data)
-                        setPool((prevState)=> [res.data, ...prevState])
-                        
+                        setPool((prevState) => [res.data, ...prevState])
+
                     })
                     .catch(err => console.log(err))
-               })
-           return true
-       }
-   }
+            })
+            return true
+        }
+    }
 
-    useEffect(()=>{
+    useEffect(() => {
 
-    getfriends()
-    },[])
+        getfriends()
+    }, [])
     console.log('friends..')
 
     console.log(friends + ' friends')
     return (
         <div className="friendPage"><h2 className="pageTitle">Your Friends</h2>
-            {isLoading 
-            ?(
-                <>
-                <div className="friendPageSearch"><SearchBar></SearchBar></div>
-                <>
-                {
-                    pool.map(friend =>
-                    (
-                        <FriendCard  key ={friend._id}  {...props} data ={friend}></FriendCard>
+            {isLoading
+                ? (
+                    <>
+                        <div className="friendPageSearch"><SearchBar></SearchBar></div>
+                        <>
+                            {
+                                pool.map(friend =>
+                                (
+                                    <FriendCard key={friend._id}  {...props} data={friend}></FriendCard>
 
-                    )
-                    
-                    )
-                }
-                </>
-                </>
-            )
-            :(
-                <h3>...Loading</h3>
-            )
+                                )
+
+                                )
+                            }
+                        </>
+                    </>
+                )
+                : (
+                    <h3>...Loading</h3>
+                )
             }
         </div>
     )
