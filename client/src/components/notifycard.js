46365712543337a0a3436ec1d237b16b036e643b
Michelle <35238012+MissWibbon@users.noreply.github.com>
2021-06-19 19:26:04 -0500

    updated form notifiction format

--- client/src/components/notifycard.js
+++ client/src/components/notifycard.js
@@ -10,31 +10,31 @@ const NotifyCard = (props) =>{
     const context = useContext(RaceContext)
     const {localUser, setStats} = context
     const [player, setPlayer] = useState({})
+    const hours = props.data.hours
+    if(hours < 1) {
+        hours = ""
+    } else if(hours < 2) {
+        hours = hours + "hour"
+    } else {
+        hours = hours + "hours"
+    }
+    const seconds = props.data.seconds
+    if(seconds < 1) {
+        seconds = ""
+    } else if(seconds < 2) {
+        seconds = seconds + "second"
+    } else {
+        seconds = seconds + "seconds"
+    }
+    const minutes = props.data.minutes
+    if(minutes < 1) {
+        minutes = ""
+    } else if(minutes < 2) {
+        minutes = minutes + "minute"
+    } else {
+        minutes = minutes + "minutes"
+    }
     useEffect(() => {
-        const hours = props.data.hours
-        if(hours < 1) {
-            hours = ""
-        } else if(hours < 2) {
-            hours = hours + "hour"
-        } else {
-            hours = hours + "hours"
-        }
-        const seconds = props.data.seconds
-        if(seconds < 1) {
-            seconds = ""
-        } else if(seconds < 2) {
-            seconds = seconds + "second"
-        } else {
-            seconds = seconds + "seconds"
-        }
-        const minutes = props.data.minutes
-        if(minutes < 1) {
-            minutes = ""
-        } else if(minutes < 2) {
-            minutes = minutes + "minute"
-        } else {
-            minutes = minutes + "minutes"
-        }
         
             API.getOneUser(props.data.requestor)
                 .then(res => {
@@ -55,7 +55,7 @@ const NotifyCard = (props) =>{
         <div className="challenge-response">
         
         <div className="challenge-label"><img className="challengePlayerImg" src={player.image}/><Link className="opponentName" to ={`users/${props.data.requestor}`}>@{player.userName} </Link> has challenged you to race for 
-            <div className="distance">{hours}, {minutes} minute, {seconds} seconds</div>
+            <div className="distance">{hours}, {minutes} minute, {props.data.seconds} seconds</div>
         </div>
         <div className="message">Their message to you: {props.message}</div>
         <div className="responseButtonWrap">
