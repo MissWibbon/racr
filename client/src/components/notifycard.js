64ce6f84803267f5b983400415b7cf3e90dc8dfb
Michelle <35238012+MissWibbon@users.noreply.github.com>
2021-06-19 19:36:22 -0500

    updated form notifiction format

--- client/src/components/notifycard.js
+++ client/src/components/notifycard.js
@@ -56,7 +56,7 @@ const NotifyCard = (props) =>{
         
         <div className="challenge-label"><img className="challengePlayerImg" src={player.image}/><Link className="opponentName" to ={`users/${props.data.requestor}`}>@{player.userName} </Link> has challenged you to race for 
             <div className="timechallenge">{hours}, {minutes}, {seconds}</div>
-            <div className="distance">{miles}, {kilometers}</div>
+            <div className="distance">{props.data.miles}, {props.data.kilometers}</div>
         </div>
         <div className="message">Their message to you: {props.message}</div>
         <div className="responseButtonWrap">
